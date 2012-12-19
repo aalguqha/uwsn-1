df2213fdf1af3e93ed7b685b2ce715f684c41bb7
Yang Shao <tjushaoyang@yahoo.cn>
2012-12-18 17:20:18 -0800

    add new_version

---
+++ new_version/underwaterpropagation.h
@@ -0,0 +1,48 @@
+
+
+#ifndef ns_underwaterpropagation_h
+#define ns_underwaterpropagation_h
+
+
+
+#define SPEED_OF_SOUND_IN_WATER	1500  //actual speed of sound in water
+
+#include <topography.h>
+#include <packet-stamp.h>
+#include "underwaterphy.h"
+
+class PacketStamp;
+class UnderwaterPhy;
+
+class UnderwaterPropagation : public TclObject {
+
+public:
+  UnderwaterPropagation() : name(NULL), topo(NULL) {}
+
+  // calculate the Pr by which the receiver will get a packet sent by
+  // the node that applied the tx PacketStamp for a given inteface 
+  // type
+  
+  double  Attenuation(double, double, double);
+  double  Thorp(double);
+  
+  /*	Added by Michael E. Zuba and Yishan Su
+  		Shallow Water Model
+  		::Begin::
+  */
+  double RogersModel(double, double, double, double, double);
+  double Noise(double, double, double);
+  double SNR(double, UnderwaterPhy *);
+  /*
+  		::End::
+  */
+  virtual double Pr(PacketStamp *tx, PacketStamp *rx, UnderwaterPhy *);
+  virtual int command(int argc, const char*const* argv);
+
+protected:
+  char *name;
+  Topography *topo;
+};
+
+#endif /* __underwaterpropagation_h__ */
+
